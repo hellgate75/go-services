382a1c811b4b1fa36d152ad2f88ce0f1d4e42bea
hellgate75 <hellgate75@gmail.com>
2020-05-20 21:20:25 +0100

    Adding MongoDb support for database clusters

--- database/mongodb/driver.go
+++ database/mongodb/driver.go
@@ -7,8 +7,8 @@ import (
 	"github.com/hellgate75/go-services/database"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"strings"
 )
-
 
 type mongoDriver struct {
 }
@@ -16,15 +16,67 @@ type mongoDriver struct {
 func (md *mongoDriver) Connect(config database.DbConfig) (database.Connection, error) {
 	var client *mongo.Client
 	var err error
-	defer func(){
-		if r:=recover(); r != nil {
+	defer func() {
+		if r := recover(); r != nil {
 			err = errors.New(fmt.Sprintf("Mongo-Driver::Connect %v", r))
 		}
 	}()
 	if config.Name != "" && config.Password != "" {
-		client, err = mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%v", config.Name, config.Password, config.Host, config.Port) ))
+		if strings.Index(config.Host, ":") > 0 {
+			if strings.Index(config.Host, ",") > 0 {
+				//Cluster compressed configuration
+				var clusterURI = ""
+				for _, srv := range strings.Split(config.Host, ",") {
+					lst := strings.Split(srv, ":")
+					xhost, xport := lst[0], lst[1]
+					var uri = fmt.Sprintf("%s:%s@%s:%s", config.Name, config.Password, xhost, xport)
+					var sep = ""
+					if len(clusterURI) > 0 {
+						sep = ","
+					}
+					clusterURI = fmt.Sprintf("%s%s%s", clusterURI, sep, uri)
+				}
+				clusterURI = fmt.Sprintf("mongodb://%s", clusterURI)
+				client, err = mongo.NewClient(options.Client().ApplyURI(clusterURI))
+			} else {
+				//Single server compressed configuration
+				lst := strings.Split(config.Host, ":")
+				xhost, xport := lst[0], lst[1]
+				var uri = fmt.Sprintf("mongodb://%s:%s@%s:%s", config.Name, config.Password, xhost, xport)
+				client, err = mongo.NewClient(options.Client().ApplyURI(uri))
+			}
+		} else {
+			//No compressed configuration
+			client, err = mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%v", config.Name, config.Password, config.Host, config.Port)))
+		}
 	} else {
-		client, err = mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%v", config.Host, config.Port) ))
+		if strings.Index(config.Host, ":") > 0 {
+			if strings.Index(config.Host, ",") > 0 {
+				//Cluster compressed configuration
+				var clusterURI = ""
+				for _, srv := range strings.Split(config.Host, ",") {
+					lst := strings.Split(srv, ":")
+					xhost, xport := lst[0], lst[1]
+					var uri = fmt.Sprintf("%s:%s", xhost, xport)
+					var sep = ""
+					if len(clusterURI) > 0 {
+						sep = ","
+					}
+					clusterURI = fmt.Sprintf("%s%s%s", clusterURI, sep, uri)
+				}
+				clusterURI = fmt.Sprintf("mongodb://%s", clusterURI)
+				client, err = mongo.NewClient(options.Client().ApplyURI(clusterURI))
+			} else {
+				//Single server compressed configuration
+				lst := strings.Split(config.Host, ":")
+				xhost, xport := lst[0], lst[1]
+				var uri = fmt.Sprintf("mongodb://%s:%s", xhost, xport)
+				client, err = mongo.NewClient(options.Client().ApplyURI(uri))
+			}
+		} else {
+			//No compressed configuration
+			client, err = mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%v", config.Host, config.Port)))
+		}
 	}
 	if err != nil {
 		return nil, err
@@ -35,16 +87,24 @@ func (md *mongoDriver) Connect(config database.DbConfig) (database.Connection, e
 		return nil, err
 	}
 	var conn = mongoConnection{
-		Valid: (client != nil),
-		Client: client,
-		Context: &ctx,
-		Cancel: cancel,
+		Valid:         (client != nil),
+		Client:        client,
+		Context:       &ctx,
+		Cancel:        cancel,
 		Configuration: config,
 	}
 	return &conn, err
 }
 
-func GetMongoDriver() database.Driver{
-	return &mongoDriver{
+func GetMongoDriver() database.Driver {
+	return &mongoDriver{}
+}
+
+func GetMongoDbConfig(host string, port int, username string, password string) database.DbConfig {
+	return database.DbConfig{
+		Host:     host,
+		Port:     port,
+		Name:     username,
+		Password: password,
 	}
 }
