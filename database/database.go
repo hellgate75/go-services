99703a457c4965f393fd2a0c084791590a24940a
hellgate75 <hellgate75@gmail.com>
2020-05-21 02:48:22 +0100

    Adding code comments for mongodb service driver

--- database/database.go
+++ database/database.go
@@ -2,86 +2,146 @@ package database
 
 import "go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
 
+// Operation enumeration type
 type Operation byte
+
+// DataType enumeration type
 type DataType byte
+
+// DriverType enumeration type
 type DriverType byte
 
 const (
-	Equals			Operation = iota + 1
+	// Equality comparator Operation enumeration type
+	Equals Operation = iota + 1
+	// Less comparator Operation enumeration type
 	LessThan
+	// Grater comparator Operation enumeration type
 	GraterThan
+	// Similitude comparator Operation enumeration type
 	Like
+	// List items comparator Operation enumeration type
 	In
-	Not				Operation = 50
-	StringType		DataType = iota + 1
+	// Negation comparator Operation enumeration type
+	Not Operation = 50
+	// String data type DataType enumeration type
+	StringType DataType = iota + 1
+	// Numeric data type DataType enumeration type
 	NumericType
+	// Decimal data type DataType enumeration type
 	DecimalType
+	// List data type DataType enumeration type
 	ListType
+	// Structure data type DataType enumeration type
 	StructType
-	MongoDbDriver	DriverType = iota + 1
+	// MongoDb DriverType enumeration type
+	MongoDbDriver DriverType = iota + 1
 )
 
-
+//Database Configuration structure
 type DbConfig struct {
-	Name		string
-	Password	string
-	Certificate	string
-	PrivateKey	string
-	Host		string
-	Port		int
+	// MongoDb User name field
+	Name string
+	// MongoDb Password field
+	Password string
+	// MongoDb Certificate file path field
+	Certificate string
+	// MongoDb Private Key file path field
+	PrivateKey string
+	// MongoDb Host name field
+	Host string
+	// MongoDb Port field
+	Port int
 }
 
+// Field descriptor structure
 type Field struct {
-	Name	string
-	Type	string
-	Size	int64
-	Precision	int
+	// Field name
+	Name string
+	// Field type
+	Type string
+	// Field size
+	Size int64
+	// Field precision
+	Precision int
 }
 
+// Value descriptor structure
 type Value struct {
-	Type	DataType
-	Value	interface{}
+	// Value Type
+	Type DataType
+	// Value Content
+	Value interface{}
 }
 
+// Condition descriptor structure
 type Condition struct {
-	Field		string
-	Operation	Operation
-	Value		Value
+	// Condition Field name
+	Field string
+	// Condition operation type
+	Operation Operation
+	// Comparison Value or RegExp expression
+	Value Value
 }
 
+// Command Single Row Result descriptor structure
 type Result struct {
-	Columns		int64
-	Values		[]interface{}
-	Document	bsoncore.Document
+	// Number of Columns
+	Columns int64
+	// Column Values
+	Values []interface{}
+	// Record Value
+	Document bsoncore.Document
 }
 
-type ResultSet struct{
-	Lines		int64
-	Records		[]Result
+// Result Set descriptor structure
+type ResultSet struct {
+	// Number of lines
+	Lines int64
+	// Results Records
+	Records []Result
 }
 
+// Data Reference descriptor structure
 type DataRef struct {
-	Database	string
-	Namespace	string
-	FieldSetRef	string
+	// Database name
+	Database string
+	// Namespace value
+	Namespace string
+	// Field Set Reference
+	FieldSetRef string
 }
 
+// Connection interface
 type Connection interface {
+	// Execute Query on the database instance
 	Query(dbRef DataRef, fields []string, conditions []Condition) (ResultSet, error)
-	Insert(dbRef DataRef, fields  []Field, values[]Value) error
-	Update(dbRef DataRef, conditions []Condition, fields  []Field, values[]Value) (int64, error)
+	// Insert record on the database instance
+	Insert(dbRef DataRef, fields []Field, values []Value) error
+	// Update one or more records on the database instance
+	Update(dbRef DataRef, conditions []Condition, fields []Field, values []Value) (int64, error)
+	// Delete one or more records on the database instance
 	Delete(dbRef DataRef, conditions []Condition) (int64, error)
+	// Purge one or more records on the database instance
 	Purge(dbRef DataRef) (int64, error)
+	// Create Namespace, Collection or Entity element on the database instance
 	Create(dbRef DataRef, fields []Field) error
+	// Create new database instance
 	CreateDb(dbRef DataRef) error
+	// Drop Namespace, Collection or Entity element on the database instance
 	Drop(dbRef DataRef) error
+	// Drop existing database instance
 	DropDb(dbRef DataRef) error
+	// Close connection
 	Close() error
+	// Is connection open
 	IsConnected() bool
-	GetLastError()	error
-
+	// Get latest execution error
+	GetLastError() error
 }
 
+// Driver interface
 type Driver interface {
+	// Connect to a database instance or database cluster instance
 	Connect(config DbConfig) (Connection, error)
 }
